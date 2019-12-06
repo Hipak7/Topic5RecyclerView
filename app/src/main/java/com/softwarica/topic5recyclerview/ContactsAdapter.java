04febedca1ecd4cd22469eca896769289464886d
hipak <hpk393@gmail.com>
2019-12-06 14:19:32 +0545

    RecyclerView

---
+++ app/src/main/java/com/softwarica/topic5recyclerview/ContactsAdapter.java
@@ -0,0 +1,19 @@
+package com.softwarica.topic5recyclerview;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ContactsAdapter {
+    public class ContactsViewHolder extends RecyclerView.ViewHolder{
+        ImageView imgProfile;
+        TextView tvName,tvPhone;
+        public ContactsViewHolder(@NonNull View itemView){
+            super(itemView);
+            imgProfile=itemView.findViewById(R.id.imgProfile);
+        }
+    }
+}
