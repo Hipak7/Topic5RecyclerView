e962d97381d4c8ee1ef8aa12714059cb18d48e56
hipak <hpk393@gmail.com>
2019-12-06 14:59:43 +0545

    RecyclerViewContact Class

--- app/src/main/java/com/softwarica/topic5recyclerview/ContactsAdapter.java
+++ app/src/main/java/com/softwarica/topic5recyclerview/ContactsAdapter.java
@@ -1,13 +1,41 @@
 package com.softwarica.topic5recyclerview;
 
+import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ContactsAdapter {
+import java.util.List;
+
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ContactsViewHolder>{
+    Context mcontext;
+    List<Contacts> contactsList;
+
+    public ContactsAdapter(Context mcontext, List<Contacts> contactsList) {
+        this.mcontext = mcontext;
+        this.contactsList = contactsList;
+    }
+
+    @NonNull
+    @Override
+    public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactsViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactsList.size();
+    }
+
     public class ContactsViewHolder extends RecyclerView.ViewHolder{
         ImageView imgProfile;
         TextView tvName,tvPhone;
@@ -16,5 +44,6 @@ public class ContactsAdapter {
             imgProfile=itemView.findViewById(R.id.imgProfile);
             //asdasd
         }
+
     }
 }
